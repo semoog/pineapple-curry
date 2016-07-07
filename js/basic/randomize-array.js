1eb155c8acc320e5b84fa6c9c2251086fa7350e5
Sebastian Ernst <sebmernst@gmail.com>
2016-07-07 09:16:46 -0600

    pig-latin

--- js/basic/randomize-array.js
+++ js/basic/randomize-array.js
@@ -14,3 +14,17 @@ var students = [
 , 'David'
 , 'Mark'
 ]
+
+function shuffle(array) {
+  let i = 0, j = 0, temp = null
+
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i] // last element stored for later
+    array[i] = array[j] // last element --> random element
+    array[j] = temp // random element --> last element
+  }
+  return array
+}
+
+console.log(shuffle(students))
--- js/intermediate/pig-latin.js
+++ js/intermediate/pig-latin.js
@@ -1,1 +1,15 @@
 // write a function that translates english to pig-latin
+
+function pigLatin(str) {
+	var str = str.split(' ');
+	for (var i = 0; i < str.length; i++) {
+		var word = str[i].split('');
+		var firstLetter = word.shift();
+		word.push(firstLetter);
+		word = word.join('') + 'ay';
+		str[i] = word;
+	}
+	return str.join(' ');
+}
+
+console.log(pigLatin('this is a pig latin string'));
